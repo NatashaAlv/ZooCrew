68f26e369242d243147b4060371bb841e857c2ea
Natasha Alvarado Dubkova <alvarnat@sheridancollege.ca>
2026-04-09 23:56:58 +0000

    Adjust camera distance in centerModelCamera function for better model framing

--- sketch.js
+++ sketch.js
@@ -112,7 +112,7 @@ function centerModelCamera(model){
   const size = box.getSize(new THREE.Vector3());
   const maxDim = Math.max(size.x, size.y, size.z);
   const fov = camera.fov * (Math.PI / 180);
-  let cameraZ = Math.abs(maxDim / 2 / Math.tan(fov / 2)) * 2;
+  let cameraZ = Math.abs(maxDim / 2 / Math.tan(fov / 2)) * 1.5;
 
   camera.position.set(center.x, center.y, center.z + cameraZ);
   controls.target.copy(center);
