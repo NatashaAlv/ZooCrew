984ddac37fad078134995c94b5daa43196cd4d44
Natasha Alvarado Dubkova <alvarnat@sheridancollege.ca>
2026-04-09 23:33:06 +0000

    Update model loading from giraffe to monkeyTwo in the loader

--- sketch.js
+++ sketch.js
@@ -49,7 +49,7 @@ function init(){
   loader = new GLTFLoader();
 
   // Load models
-  loader.load('models/animals/giraffe.glb', (gltf) => {
+  loader.load('models/animals/monkeyTwo.glb', (gltf) => {
   giraffeModel = gltf.scene;
   giraffeModel.scale.set(1,1,1);
   // currentModel = giraffeModel;
